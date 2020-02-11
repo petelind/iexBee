fb6be3152593ddc1c41d8fb54610f4bca8bedeb6
Denis Petelin <d.petelin@icloud.com>
2020-02-10 18:25:35 -0600

    Quick intro into the testing added.

---
+++ persistence/test_dynamostore.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+
+class TestDynamoStore(TestCase):
+    def test_store_documents_PassValidDocs_ExpectThemAppearInDB(self):
+        # ARRANGE:
+
+        # ACT:
+
+        # ASSERT:
+
+        self.assertTrue(True)
